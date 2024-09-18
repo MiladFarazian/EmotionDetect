6b242372dbd00a87deee0d53cafa9e70e0a3d32a
Milad Farazian <mfarazia@ucsc.edu>
2024-09-17 23:17:37 -0700

    fix script?

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 const video = document.getElementById("video");
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
-  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-  faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+  faceapi.nets.tinyFaceDetector.loadFromUri("/EmotionDetect/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/EmotionDetect/models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("/EmotionDetect/models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("/EmotionDetect/models"),
 ]).then(startVideo);
 
 function startVideo() {
